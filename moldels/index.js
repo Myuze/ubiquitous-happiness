de996e0b70d172a9d3881591364f3614c05bc1ce
Mike Rogers <mrogers23458@gmail.com>
2021-09-29 06:08:00 -0700

    Created directory models. Added user.js, post.js, data.js to models. Updated models with properties required in ticket.

---
+++ moldels/index.js
@@ -0,0 +1,24 @@
+const User = require("./user")
+const Post = require("./post")
+const Data = require("./data")
+
+User.hasMany(Post, {
+    foreignKey: "author_id",
+    onDelete: "CASCADE",
+  });
+  
+Post.belongsTo(User, {
+    foreignKey: "author_id",
+  });
+
+User.hasOne(Data, {
+    foreignKey: "data_id",
+    onDelete: "CASCADE"
+});
+
+Data.belongsTo(User, {
+    foreignKey: "data_id",
+    onDelete: "CASCADE"
+})
+
+  module.exports = { User, Post, Data };
