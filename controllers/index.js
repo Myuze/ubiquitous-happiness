e2c614209d19efe704b3a8b2a83023868c46c9e7
Myuze <Myuze@users.noreply.github.com>
2021-09-30 20:56:06 -0700

    Added base forum route

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const forumRoutes = require('./api/forum-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/forum', forumRoutes);
 
 module.exports = router;
