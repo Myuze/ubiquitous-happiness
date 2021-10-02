ca7489af73aaaf73d0e0b0fe010f4b472841d833
Mike Rogers <mrogers23458@gmail.com>
2021-10-01 19:11:02 -0700

    added a get route for user to be able to view the page where they can make a new post. Added userRoutes to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const forumRoutes = require('./api/forum-routes');
+const userRoutes = require('./user-routes')
 
 router.use('/', globalController);
 router.use('/api', apiRoutes);
 router.use('/forum', forumRoutes);
+router.use('/home', homeRoutes)
+router.use('/user', userRoutes)
 
 module.exports = router;
