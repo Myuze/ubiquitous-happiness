6d731ce2a8ee0cf73e3ae8494a4b2f4a57cc00ad
Myuze <Myuze@users.noreply.github.com>
2021-10-04 00:09:12 -0700

    Added carousel nav partial

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,7 +1,7 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('carousel__button--right');
-const prevButton = document.querySelector('carousel__button--left');
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
 const dotsNav = document.querySelector('.carousel__nav');
 const dots = Array.from(dotsNav.children);
 
@@ -15,6 +15,10 @@ const setSlidePostition = (slide, index) => {
 slides.forEach(setSlidePostition);
 
 nextButton.addEventListener('click', e => {
-  currentSlide = track.document.querySelector('.current-slide');
+  const currentSlide = track.document.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
 
+  // Move to the next slide
+  track.style.transform = `translateX(${amountToMove}')`;
 });
