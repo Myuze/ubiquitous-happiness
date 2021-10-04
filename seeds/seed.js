008421b8df0f9b4ea8dee49697e28f727e738963
Florian Meneses <Myuze@users.noreply.github.com>
2021-10-03 18:22:40 -0700

    Update seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User , Game, GameCategory } = require('../models');
+const { User, Game, GameCategory } = require('../models');
 
 const userData = require('./userData.json');
 const gameData = require('./gameData.json');
