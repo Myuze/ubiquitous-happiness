7c4cc99290bce6dbe436f85d09a726133c08900f
Myuze <Myuze@users.noreply.github.com>
2021-10-04 01:34:06 -0700

    Added Left button functionality

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -15,13 +15,35 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = `translateX(-${targetSlide.style.left})`;
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+// When I click the Left Button, slides move left
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+
+  moveToSlide(track, currentSlide, prevSlide);
+});
+
+// When I click the Right Button, slides move right
 nextButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
   const nextSlide = currentSlide.nextElementSibling;
-  const amountToMove = nextSlide.style.left;
 
   // Move to the next slide
-  track.style.transform = `translateX(-${amountToMove})`;
-  currentSlide.classList.remove('current-slide');
-  nextSlide.classList.add('current-slide');
+  moveToSlide(track, currentSlide, nextSlide);
+});
+
+// When I click on the nav indicators, navigate to that slide
+dotsNav.addEventListener('click', e => {
+  const targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector('.current-slide');
+  const currentDot = dotsNav.querySelector('.current-slide');
 });
