75e3a7f494e3a6e51e9e872ff5453361ed8f3122
Myuze <Myuze@users.noreply.github.com>
2021-10-04 02:05:57 -0700

    Added navigation based on nav buttons

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -15,27 +15,58 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
+// Move to target slide
 const moveToSlide = (track, currentSlide, targetSlide) => {
   track.style.transform = `translateX(-${targetSlide.style.left})`;
   currentSlide.classList.remove('current-slide');
   targetSlide.classList.add('current-slide');
 };
 
+// Update Nav Dot info
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide');
+  targetDot.classList.add('current-slide');
+};
+
+// Hide or show arrow dependent on where the user is in the list
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.add('is-hidden');
+  } else {
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+  }
+};
+
 // When I click the Left Button, slides move left
 prevButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
   const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
   moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevButton, nextButton, prevIndex);
 });
 
 // When I click the Right Button, slides move right
 nextButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
   const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector('.current-slide');
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex(slide => slide === nextSlide);
 
   // Move to the next slide
   moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
 // When I click on the nav indicators, navigate to that slide
@@ -46,4 +77,10 @@ dotsNav.addEventListener('click', e => {
 
   const currentSlide = track.querySelector('.current-slide');
   const currentDot = dotsNav.querySelector('.current-slide');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);  
+  hideShowArrows(slides, prevButton, nextButton, targetIndex);
 });
