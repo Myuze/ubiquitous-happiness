aec408addf642df40bbbd37d06625bf88afe3558
Myuze <Myuze@users.noreply.github.com>
2021-10-05 20:01:23 -0700

    Added game-routes

---
+++ controllers/api/game-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Game } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbGameData = await Game.findAll();
+    
+    res.status(200).json( dbGameData );
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error'});
+  }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const dbGameData = await Game.findByPk({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json;
+  } catch (err) {
+    res.status(500).render('404');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const dbGameData = await Game.create(req.body);
+
+    console.log(dbGameData);
+
+    res.status(200).json(dbGameData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const dbGameData = await Game.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!dbGameData) {
+      res.status(404).json({ message: 'No Game found with this id!' });
+      return;
+    }
+
+    res.status(200).json(dbGameData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
