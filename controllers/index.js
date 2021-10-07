8130d965b8a6bd4e9eac04fb8b9901762786f4fa
Myuze <Myuze@users.noreply.github.com>
2021-10-07 14:14:40 -0700

    Added leaderboard route, and coming soon image.

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const forumRoutes = require('./api/forum-routes');
 const userRoutes = require('./user-routes');
+const leaderboardRoutes = require('./leaderboard-routes');
 const registerRoutes = require('./register-routes');
 
 router.use('/api', apiRoutes);
 router.use('/forum', forumRoutes);
 router.use('/user', userRoutes);
+router.use('/leaderboard', leaderboardRoutes);
 router.use('/register', registerRoutes);
 router.use('*', homeRoutes);
 
