9edfd418a7446583f0ac6a28e9680a91ec232f99
LordLowdar <84937115+LordLowdar@users.noreply.github.com>
2021-10-07 19:15:03 -0700

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,7 +15,7 @@ router.post('/register', async (req, res) => {
 
     req.session.save(() => {
       req.session.loggedIn = true;
-      const { user } = req.session;
+      
       res.status(200).json(dbUserData);
     });
   } catch (err) { 
