476578822c5a7492f22cdc4cd7acc9dbdde3ceb4
Myuze <Myuze@users.noreply.github.com>
2021-10-07 17:31:11 -0700

    Made bootstrap imports more lightweight, restored missing signout.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,19 +3,15 @@ const { Game } = require('../models');
 
 // Get games to display on Home 
 router.get('/', async (req, res) => {
+  const { loggedIn } = req.session;
   const gameData = await Game.findAll()
-  .catch((err) => { 
-    res
-      .status(500)
-      .json(err)
-      .render('404');
-  });
+    .catch((err) => { 
+      res.status(500).json(err).render('404');
+    });
   
   const games = gameData.map((game) => game.get({ plain: true }));
   
-  res
-    .status(200)
-    .render('home', { games });
+  res.status(200).render('home', { games, loggedIn });
 });
 
 module.exports = router;
